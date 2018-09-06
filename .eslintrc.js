1b7387ad410067e848fab359f3c4e60e3d9459ff
tania tello <tatello@MacBook-Pro-de-tania.local>
2018-09-06 00:02:49 -0500

    feature/First-commit-config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+{
+
+	"parserOptions": {
+	"ecmaVersion": 6,
+		"sourceType": "module",
+		"ecmaFeatures": {
+		"jsx": true
+		}
+	},
+
+
+    "rules": {
+
+        "indent": [2, "tab"],
+        "quotes": [2,"single"]
+    },
+    "extends": "eslint:recommended"
+
+}
